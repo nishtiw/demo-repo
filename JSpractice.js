69ff2aac1f94c5e205c146adf44457bd2d9a2ca9
Nisha Tiwari <nishatiwari1719@gmail.com>
2022-05-25 11:34:43 +0530

    All files committed

---
+++ JSpractice.js
@@ -0,0 +1,8 @@
+const firstName = "Nisha";
+const lastName = "Tiwari";
+const age = 23;
+
+// console.log(`I am ${firstName} ${lastName}. 
+// I am ${age} years old.`)
+
+console.log('I am '+firstName+' '+lastName+'.\n'+'I am '+age+' years old.')
